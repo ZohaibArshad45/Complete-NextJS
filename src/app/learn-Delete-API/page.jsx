a29eb7f6e35836686d1425c402e0592993a648ad
Zohaib Arshad <zohaibfta@gmail.com>
2025-03-22 17:09:12 +0500

    Delete API Code only

--- src/app/learn-Delete-API/page.jsx
+++ src/app/learn-Delete-API/page.jsx
@@ -17,10 +17,8 @@ export const DELETE = async (req, { params }) => {
     return (
         <div>
             <h1 className="text-2xl font-bold text-red-600">🗑️ Learn DELETE API in Next.js</h1>
-            <p className="text-gray-700 mt-4">🚀 DELETE API is used in **dynamic routes**.</p>
-            <p> For check Paste it in POSTMAN <br />http://localhost:3000/api/getApi/3</p>
-            <br />
-
+            <p>ya b POSTMAN check ho ga</p>
+            
             <h2 className="text-lg font-semibold text-gray-600">📜 Example DELETE API Route Code:</h2>
             <pre className="bg-gray-800 text-white w-full rounded-lg p-2 overflow-x-auto">
                 <code>{deleteAPI}</code>
