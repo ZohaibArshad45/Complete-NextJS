1241007293bb703f8af4b08278ad07624624dac8
Zohaib <zohaibfta@gmail.com>
2025-07-03 11:47:16 +0500

    update

--- src/database/connection/ConnectionString.js
+++
@@ -1,7 +0,0 @@
-const MongoURL = process.env.MONGODB_URL;
-
-if (!MongoURL) {
-    throw new Error("MongoDB URL is missing. Please set the MONGODB_URL environment variable.");
-}
-
-export default MongoURL;
